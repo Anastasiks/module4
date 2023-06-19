d4574aa83923048dcc8da7a9c9b6d1352fd95031
ANASTASIA <nastamolokanova96312@gmail.com>
2023-06-19 17:03:38 +0300

    'initial'

---
+++ Module4.py
@@ -0,0 +1,22 @@
+# a = input()
+# for i in a:
+#     if i == a[len(a)-1]:
+
+def strcounter(s):
+    for i in set(s):
+        counter = 0
+        for y in s:
+            if i == y:
+                counter += 1
+        print(i, "-", counter)
+strcounter("abbcd")
+
+# Множество (set) - это структкра данных, которая содержит в себе уникальные элементы в неупорядоченом виде
+
+def stronger2(s):
+    syms_counter = {}
+    for i in s:
+        syms_counter[i] = syms_counter.get(i, 0) + 1
+    for i, count in syms_counter.items():
+        print(i, count)
+stronger2("abbs")
